24104bfa09c68dd3dba11ffa693e3e0c3ede4a18
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2026-06-27 13:41:53 +0100

    create icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import Text from "./components/Text";
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "./assets/icons/Check-Regular.svg?react";
+import XIcon from "./assets/icons/X-Regular.svg?react";
+import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
+import SpinnerIcon from "./assets/icons/Spiner.svg?react";
+import Icon from "./components/Icon";
 
 export default function App() {
   return(
@@ -6,6 +13,14 @@ export default function App() {
       <Text as="h1" variant="body-md-bold" className="text-pink-dark">
         Hello World
       </Text>
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
     </>
   ) 
 }
