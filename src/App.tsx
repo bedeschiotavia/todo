3f41d90e1ac82bcd1c16005428402f8b4420d1b2
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2026-06-27 13:53:47 +0100

    create badge component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
 import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
 import SpinnerIcon from "./assets/icons/Spiner.svg?react";
 import Icon from "./components/Icon";
+import Badge from "./components/Badge";
 
 export default function App() {
   return(
@@ -17,9 +18,13 @@ export default function App() {
         <Icon svg={TrashIcon} className="fill-green-base" />
         <Icon svg={CheckIcon} />
         <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={SpinnerIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+      <div className="flex gap-2">
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </>
   ) 
