9152bd05582b877c2b1500070c290a05cae78e01
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2026-06-27 12:52:33 +0100

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Text from "./components/Text";
+
+export default function App() {
+  return(
+    <>
+      <Text as="h1" variant="body-md-bold" className="text-pink-dark">
+        Hello World
+      </Text>
+    </>
+  ) 
+}
